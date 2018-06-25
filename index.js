95d83ddc29a30ea9b71fefebe8fc315a4e65fb41
CatStarwind <kj4cgv@gmail.com>
2018-06-25 12:24:04 -0700

    Added ignores. Created Match and Viewer objects. Added multi-channel support.

--- index.js
+++ index.js
@@ -9,6 +9,56 @@ var notifyCh = [];
 var todayGames = [];
 var now = new Date();
 var parseOdd = function(odd){ return {"name": odd[0], "color": odd[1], "p": odd[2]} }
+var Match = function(game){	
+	return {
+		mid: game.mid				//MatchID
+		,start: game.start 			//Match Start
+		,i: game.i					//Interval
+		,match: game.match			//Persistent Match Info		
+		,audience: game.audience	//Channels to scream at
+		,send: function(msg, limit=false){			
+			this.audience.forEach(function(a){				
+				if(!limit || a.chirp){
+					a.channel.send(msg);
+					if(limit) a.chirp = false;
+				}				
+			});
+		}
+		,sendOdds: function(o){
+			this.audience.forEach(function(a){				
+				if(a.refresh){
+					console.log("Sending odds to #"+a.channel.name+" in ["+a.channel.guild.name+"]");
+					a.channel.send({
+						files: [{ attachment: o,  name: 'winprob.jpg'  }]
+					}).then(msg => {
+						a.refresh = false; //Cool down
+						a.chirp = true; //Reset chirp
+						setTimeout(function(){a.refresh = true;}, 5*60*1000);
+					}).catch(err => {
+						console.log(err.name +": "+err.message+" ("+err.code+")");
+						if(err.code ===  50013) a.channel.send("I can't attach images! :(");
+					});
+				}
+			});
+		}
+		,check: function(){			
+			checkMatch(todayGames.findIndex(g => g.mid === this.mid));
+		}
+		,stop: function(i){
+			console.log("Stopping Game "+(todayGames.findIndex(g => g.mid === this.mid)+1));			
+			clearInterval(this.i);
+			this.i = null;
+			this.audience = [];	
+		}
+	}
+}
+var Viewer = function(ch){
+	return {
+		channel: ch
+		,refresh: true
+		,chirp: true
+	}
+}
 
 //Handle only current day games
 var getGames = function(){
@@ -16,15 +66,16 @@ var getGames = function(){
 	ggames.forEach(function(g){
 		var gday = new Date(g.start);
 		if(now.toJSON().split("T")[0] === gday.toJSON().split("T")[0]){
-			g.i = null; //Internval
-			g.match = { //Persistent Match Info
+			g.i = null;
+			g.match = {
 				"odds": [parseOdd(["TeamA", "#0000FF", "50"]), parseOdd(["TeamB", "#FF0000", "50"])]
 				,"score": [-1,-1]
 				,"refresh": true 
 				,"chirp": true
 			}			
-			g.audience = []; //Channels to scream at			
-	  		todayGames.push(g);
+			g.audience = [];
+
+	  		todayGames.push(Match(g));
 	  	}
 	});
 	console.log(todayGames.length + " matches found.")
@@ -37,7 +88,7 @@ var getGames = function(){
 	
 vuvu.on('ready', function() {
     console.log(vuvu.user.username + " is online!");
-    getGames();
+    getGames();    
 });
 
 vuvu.on("guildCreate", guild => {
@@ -51,32 +102,44 @@ vuvu.on('message', message => {
 	if(message.content.indexOf(config.prefix) !== 0) return;
 
 	var args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-	var cmd = args.shift().toLowerCase();	
+	var cmd = args.shift().toLowerCase();
 
 	if(cmd === "odds"){
 		g = parseInt(args[1])-1;
-		if(isNaN(g) || g < 0 || g >= todayGames.length){ message.channel.send("Please select a game! Use v!games to see todays games."); return false; }
+
+		if(isNaN(g) || g < 0 || g >= todayGames.length){ 
+			message.channel.send("Please select a game! Use v!games to see todays games.");
+			return;
+		}
 		
-		if(args[0] === "start"){
-			console.log("Adding "+message.channel.name+" in "+message.guild.name+" to audience for game "+g);
-			if(!todayGames[g].audience.find(c => c.id === message.channel.id)){
-				todayGames[g].audience.push(message.channel);
-				console.log(todayGames[g].audience.length + " now listening.");				
-			}
-			checkMatch(message.channel, g);
-			if(todayGames[g].i === null) todayGames[g].i = setInterval(function(){checkMatch(message.channel, g)}, 5*1000);			
+		if(args[0] === "start"){			
+			if(!todayGames[g].audience.find(v => v.channel.id === message.channel.id)){
+				console.log("Adding #"+message.channel.name+" in ["+message.guild.name+"] to audience for game "+(g+1));
+				todayGames[g].audience.push(Viewer(message.channel));
+				console.log(todayGames[g].audience.length + " now listening.");
+			} else {
+				message.channel.send("You're already tunned in!");
+			}	
+			if(todayGames[g].i === null) todayGames[g].i = setInterval(todayGames[g].check.bind(todayGames[g]), 5*1000);
+			
 		}
 
-		if(args[0] === "stop"){
-			console.log("Stopping Game "+g);
-			message.channel.send("I told you to never tell me the odds.");
-			clearInterval(todayGames[g].i);
-			todayGames[g].i = null;
+		if(args[0] === "stop"){			
+			var i = todayGames[g].audience.findIndex(v => v.channel.id  === message.channel.id);			
+
+			if(i >= 0) {
+				console.log("Removing #"+message.channel.name+" in ["+message.guild.name+"] to audience for game "+(g+1));
+				todayGames[g].audience.splice(i, 1);
+				message.channel.send("I told you to never tell me the odds.");
+				console.log(todayGames[g].audience.length + " now listening.");
+				if(todayGames[g].audience.length === 0)	todayGames[g].stop();
+			}
 		}
 
 		if(args[0] === "check"){
-			console.log("Checking Game "+g);
-			checkMatch(message.channel, g);
+			console.log("Checking Game "+(g+1)+" for #"+message.channel.name+" in ["+message.guild.name+"]");			
+			var ch = todayGames[g].audience.find(v => v.channel.id === message.channel.id);			
+			if(ch) ch.refresh = true;
 		}
 	}
 
@@ -104,35 +167,29 @@ vuvu.on('message', message => {
 	if(cmd === "ping"){
 		message.channel.send("Pong!");
 	}
-
-	if(cmd === "test"){
-		if(args[0] === "spam"){			
-			message.channel.send("Adding "+message.channel.name+" in "+message.guild.name+" to audience for game ");
-		}
-	}
 });
 
-var checkMatch = function(ch, g){
-	console.log("----------------------------");	
-	var matchURL = "https://www.google.com/async/lr_mt_fp?async=sp:2,emid:"+encodeURIComponent(todayGames[g].mid)+",ct:US,hl:en,tz:America%2FLos_Angeles,_fmt:jspb";
-	console.log(matchURL);
+var checkMatch = function(g){	
+	var matchURL = "https://www.google.com/async/lr_mt_fp?async=sp:2,emid:"+encodeURIComponent(todayGames[g].mid)+",ct:US,hl:en,tz:America%2FLos_Angeles,_fmt:jspb";	
 
 	request({url: matchURL, headers: {'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36"}}, function(error, response, body){		
-		try{ parseMatch(JSON.parse(body.substring(4)).match_fullpage, ch, g); }
-		catch(e){
-			ch.send("Bad JSON! Abort abort.");
-			console.log("Stopping Game "+g);
-			clearInterval(todayGames[g].i);
-			todayGames[g] = null;
+		try{
+			console.log("Parsing Game "+(g+1)+" ("+matchURL+")");
+			parseMatch(JSON.parse(body.substring(4)).match_fullpage, g); 			
+		}
+		catch(e){			
+			todayGames[g].stop();
 			console.log("Bad JSON");
+			console.log(e);			
 			return false;
 		}
 	});
 }
 
-parseMatch = function(gmatch, ch, g){	
+parseMatch = function(gmatch, g){	
 	//Grab from Google match_info Array
-	var match = todayGames[g].match;
+	var game = todayGames[g];
+	var match = game.match;
 	var title = gmatch[0][0];
 	var time = gmatch[1][0][22];
 	var score = gmatch[1][0][24];
@@ -141,43 +198,41 @@ parseMatch = function(gmatch, ch, g){
 		{name:gmatch[1][0][1][0][1], abv: gmatch[1][0][1][0][2]}
 		,{name:gmatch[1][0][2][0][1], abv: gmatch[1][0][2][0][2]}
 	];
-	var scorebox = "**"+team[0].abv+"** "+score[0]+" - "+score[1]+" **"+team[1].abv+"**";
-	console.log("Parsing Game "+g+": "+title);
+	var scorebox = (score !== null) ? "**"+team[0].abv+"** "+score[0]+" - "+score[1]+" **"+team[1].abv+"**" : "";	
 	
 	//Googles time array varies
 	if(time.length === 3){
-		ch.send(title+" is over! The results are "+scorebox);
-		console.log("Stopping Game "+g);
-		clearInterval(todayGames[g].i);
-		todayGames[g] = null;
+		game.send(title+" is over! The results are "+scorebox);
+		game.stop();
 		return false;
 	}
 	if(time[6] === "Half-time"){
-		if(match.chirp) ch.send("It's half-time! The score is "+scorebox);			
-		return match.chirp=false;
+		game.send("It's half-time! The score is "+scorebox, true);
+		return;
 	}
 
-	//Check for goal	
-	for(var i=0; i<match.score.length; i++){
-		if(match.score[i] !== score[i]){
-			if(match.score[i] >= 0) ch.send(team[0].name+" GOOOOOOOOOOOOOL! " + scorebox);
-			match.score[i] = score[i];
+	//Check for goal
+	if(score !== null){
+		for(var i=0; i<match.score.length; i++){
+			if(match.score[i] !== score[i]){
+				if(match.score[i] >= 0) game.send(team[i].name+" GOOOOOOOOOOOOOL! " + scorebox);
+				match.score[i] = score[i];
+			}
 		}
-	}	
+	}
 	
 	//Ensure odds exist and if refresh required
-	if(odds !== null && match.refresh){
+	if(odds !== null && game.audience.find(a => a.refresh)){
+		console.log("Generating odds ("+odds[5]+") for Game "+(g+1));
 		//Google: 1 is populated, 2 is pending.
 		if(odds[5] === 1){
 			var teamA = parseOdd(odds[1]);
 			var teamB = parseOdd(odds[2]);
 			var draw = parseOdd(odds[3]);
-			var msg = "";
-			match.refresh = false;
-			match.chirp = true; //Reset chirp
+			var msg = "";			
 
 			//Check if odds have changed
-			if(match.odds[0].p !== teamA.p || match.odds[1].p !== teamB.p){
+			if(match.odds[0].p !== teamA.p || match.odds[1].p !== teamB.p || game.audience.find(a => a.refresh)){
 				match.odds[0] = teamA;
 				match.odds[1] = teamB;
 
@@ -202,19 +257,11 @@ parseMatch = function(gmatch, ch, g){
 				winprob += '</div></body></html>'
 				var render = webshot(winprob, {siteType:'html', windowSize:{width:400, height:55}, shotOffset:{ left: 0, right: 0, top: 9, bottom: 0 }});
 				
-				ch.send({
-					files: [{ attachment: render,  name: 'winprob.jpg'  }]
-				}).catch(err => {
-					console.log(err.name +": "+err.message+" ("+err.code+")");
-					if(err.code ===  50013) ch.send("I can't attach images! :(");
-				});				
+				game.sendOdds(render);				
 			}
 		} else if(odds[5] === 2){
-			if(match.chirp) ch.send(odds[4]);
-			match.chirp = false;	
+			game.send(odds[4], true);			
 		}
-
-		setTimeout(function(){match.refresh = true;}, 5*60*1000);
 	}
 }
 
