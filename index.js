c2f88aa504887fcba594f33582876605639e445c
CatStarwind <kj4cgv@gmail.com>
2018-06-27 08:14:58 -0700

    Added title to goal richembed.

--- index.js
+++ index.js
@@ -306,7 +306,8 @@ parseMatch = function(gmatch, g){
 			if(match.score[i] !== score[i]){
 				if(match.score[i] >= 0){
 					var cantor = { "embed": { 
-						"description": team[i].name.toUpperCase() + " GOOOOOOOOOOOOOL!\n\n" + scorebox
+						"title": team[i].name.toUpperCase() + " GOOOOOOOOOOOOOL!"
+						,"description": scorebox
 						, "color": parseInt(match.odds[i].color.replace("#", "0x"), 16) 
 					}}					
 					game.send(cantor);
